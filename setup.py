355d24a1617959bcb8a59b648865eebd5f1162fb
exe.dev user <exedev@archimedes.exe.xyz>
2026-04-12 07:33:35 +0000

    Add setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="content-pipeline",
+    version="1.0.0",
+    description="CLI tool to extract tweet drafts and blog angles from agent learnings",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Archimedes (OpenSeneca Squad)",
+    url="https://github.com/OpenSeneca/content-pipeline",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'content-digest=main:main',
+        ],
+    },
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
