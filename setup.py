20b0746ee4df3f0b7d51d58496bd034db0bed884
Archimedes <archimedes@openseneca.org>
2026-05-14 02:01:36 +0000

    Release v1.3.0: Bump version and update README

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="squad-content-pipeline",
-    version="1.2.1",
+    version="1.3.0",
     description="Content Pipeline CLI for Seneca - Scans learnings/ for tweet drafts and blog angles",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
